1ba1e5bd8d493a23da5e1a130097066aa0a8943a
Jackeagle <jackeagle102@gmail.com>
2014-08-13 20:01:16 +0530

    Added GPL V3 Licence

--- device.sh
+++ device.sh
@@ -1,4 +1,25 @@
+#Android Multiple Device Supportable Rom Build Script With Selective Options.
+
+#patches are only for Samsung Galaxy Grand Duos GT-I9082 Only.
+    
+    #Copyright (C) 2014  Jackeagle
+
+    #This program is free software: you can redistribute it and/or modify
+    #it under the terms of the GNU General Public License as published by
+    #the Free Software Foundation, either version 3 of the License, or
+    #(at your option) any later version.
+
+    #This program is distributed in the hope that it will be useful,
+    #but WITHOUT ANY WARRANTY; without even the implied warranty of
+    #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    #GNU General Public License for more details.
+
+    #You should have received a copy of the GNU General Public License
+    #along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 #This Script Should Be Placed In AICP Rom Directory
+#if you want to add your devices replace the device option and make appropriate changes to your devices.
 
 echo -e "*>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<*"
 echo -e "*     Welcome To AICP Rom Development   *"
